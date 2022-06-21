405125d08ff3eddd79cd6793750bc81ccb479419
VandelayUtd <drewhadley.guitar@gmail.com>
2022-06-21 11:42:38 -0400

    added host for action_mailer

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "backend_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { 
+    host: 'https://film-fax.herokuapp.com/'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
