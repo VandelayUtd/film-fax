c071db0be5c73b1c1f4f74db74f9c53128aec55a
VandelayUtd <drewhadley.guitar@gmail.com>
2022-06-24 13:00:23 -0400

    made welcome screen mobile friendly

--- frontend/src/routes/home/home.styles.js
+++ frontend/src/routes/home/home.styles.js
@@ -8,4 +8,7 @@ export const Title = styled.h1`
     font-family: 'Tiro Devanagari Marathi', serif;
     color: white;
     font-size: 100px;
+    @media screen and (max-width: 800px) {
+        font-size: 45px;
+    }
 `
